1dfbeb5fba17a647798673845ffba98f9ef1f7f0
goshafighten <goshafighten@gmail.com>
2017-10-05 23:57:32 +0300

    Import Globalize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import 'globalize';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
